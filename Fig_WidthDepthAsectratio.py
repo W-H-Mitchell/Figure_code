bf6e2e2493571f0e167e93da55bf5318f028a7bd
W-H-Mitchell <hamishmitchell@outlook.com>
2021-01-22 15:47:42 +0000

    Update Fig_WidthDepthAsectratio.py

--- Fig_WidthDepthAsectratio.py
+++ Fig_WidthDepthAsectratio.py
@@ -26,17 +26,17 @@ axes1 = fig.add_subplot(3, 1, 1)
 axes2 = fig.add_subplot(3, 1, 2)
 axes3 = fig.add_subplot(3, 1, 3)
 # width plot
-axes1.set_ylabel('Width (m)')
+axes1.set_ylabel('Width, W (m)')
 axes1.plot(dist, elem_width, linestyle='solid',color='b', marker='s', markersize=4, linewidth=1) # square marker for channel elements
 axes1.plot(dist, c_width, linestyle='solid', color='k', marker='o', markersize=4, linewidth=1) # circle marker for complex scale
 axes1.plot(dist, sea_width, linestyle='solid', color='r', marker='D', markersize=4, linewidth=1) # diamond marker for seafloor channels
 # depth plot 
-axes2.set_ylabel('Thickness or Depth (m)')
+axes2.set_ylabel('Thickness or Depth, D (m)')
 axes2.plot(dist, elem_height,linestyle='solid',color='b', marker='s', markersize=4, linewidth=1) # square marker for channel elements
 axes2.plot(dist, c_height, linestyle='solid', color='k', marker='o', markersize=4, linewidth=1) # circle marker for complex scale
 axes2.plot(dist, sea_height, linestyle='solid', color='r', marker='D', markersize=4, linewidth=1) # diamond marker for seafloor channels
 # aspect plot
-axes3.set_ylabel('Thickness or Depth (m)')
+axes3.set_ylabel('Aspect Ratio (W/D)')
 axes3.set_xlabel('Downstream Distance (km)')
 axes3.plot(dist, elem_asp,linestyle='solid',color='b', marker='s', markersize=4, linewidth=1) # square marker for channel elements
 axes3.plot(dist, c_asp, linestyle='solid', color='k', marker='o', markersize=4, linewidth=1) # circle marker for complex scale
